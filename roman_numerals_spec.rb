62abecbe7fb0b324b1ff0067543fd3b67c03029e
Jamie Wright <jamie@brilliantfantastic.com>
2013-07-18 19:51:39 -0400

    GREEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEENNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN

--- roman_numerals_spec.rb
+++ roman_numerals_spec.rb
@@ -1,10 +1,10 @@
 require 'rspec'
 
 class RomanNumeral
-  @@more  = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [50, 'L'], [40, 'XL'], [10, 'X'], [5, 'V'], [4, 'IV'], [1, 'I']]
+  @@conversions  = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'], [100, 'C'], [90, 'XC'], [50, 'L'], [40, 'XL'], [10, 'X'], [9, 'IX'], [5, 'V'], [4, 'IV'], [1, 'I']]
   def self.convert number
     roman = ""
-    @@more.each do |(a,b)|
+    @@conversions.each do |(a,b)|
       blah = number / a
       roman += b * blah
       number -= blah * a
